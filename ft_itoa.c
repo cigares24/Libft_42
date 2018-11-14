1fba898b4d45b5fcd9f3cfe252296f5ed6db8ede
Axel SICAT <cigares24@yahoo.fr>
2018-11-14 13:09:39 -0800

    rendu des dernieres fonctions

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,18 +1,72 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asicat <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/12 11:20:44 by asicat            #+#    #+#             */
-/*   Updated: 2018/11/12 12:48:37 by asicat           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include "libft.h"
-
-char	*ft_itoa(int n)
+int static	malloc_size(int nb)
 {
+	int	n;
+	int	res;
 
+	n = nb;
+	res = 0;
+	if (n == 0)
+		return (2);
+	if (n < 0)
+	{
+		n = -n;
+		res++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		res++;
+	}
+	return (res + 1);
+}
+
+char static	*strrev(char *str)
+{
+	int	n;
+	int	i;
+	char	tmp;
+
+	n = 0;
+	i = strlen(str) - 1;
+	while (n < i)
+	{
+		tmp = str[n];
+		str[n] = str[i];
+		str[i] = tmp;
+		n++;
+		i--;
+	}
+	return (str);
+}
+
+char	*ft_itoa(int nb)
+{
+	int	n;
+	char	*str;
+
+	n = 0;
+	if (!(str = (char*)malloc(sizeof(char) * malloc_size(nb))))
+		return (NULL);
+	if (nb < 0)
+	{
+		nb = -nb;
+		str[malloc_size(nb) - 1] = '-';
+	}
+	if (nb == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	while (nb > 0)
+	{
+		str[n] = (nb % 10) + 48;
+		nb = nb / 10;
+		n++;
+	}
+	str = strrev(str);
+	return (str);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,13 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asicat <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/09 13:04:01 by asicat            #+#    #+#             */
-/*   Updated: 2018/11/09 13:11:38 by asicat           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <libft.h>
 
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*srcc;
+	char	*dstc;
+	size_t	i;
 
+	i = -1;
+	srcc = (char *)src;
+	dstc = (char *)dst;
+	if (srcc < dstc)
+		while ((int)(--len) >= 0)
+			*(dstc + len) = *(srcc + len);
+	else
+		while (++i < len)
+			*(dstc + i) = *(srcc + i);
+	return (dst);
+}
